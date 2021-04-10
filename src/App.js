78f2c19b5383283a108c4820e0a41ae8e9bf1439
stevannn <stewica@live.com>
2021-04-10 10:44:20 +0200

    add some dummy json and some ui components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import './theme/main.scss';
 import Input from "./components/UI/Input/Input";
 import Button from "./components/UI/Button/Button";
+import Dialog from "./components/UI/Dialog/Dialog";
 
 class App extends Component {
+
+    state = {
+        open : true
+    }
+
 
     render() {
 
         return (
-            <div className="App">
+            <div className="App" id={"app"}>
                 <Input />
                 <Button>Steva</Button>
                 <Button primary>Steva</Button>
                 <Button primary look={"flat"}> Steva</Button>
+                <Dialog
+                    open={this.state.open}
+                    title="Book details"
+                    close={ () => this.setState({ open: false }) }
+                    footerActionLabel={"Close"}
+                >
+                </Dialog>
             </div>
         );
     }
